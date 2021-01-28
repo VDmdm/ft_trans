13c1f73d02f0f835011e43e5c6b561de51ec32c7
deddara <deddara@student.21-school.ru>
2021-01-28 04:42:02 +0300

    links added

--- rails/pingpong/app/controllers/profiles_controller.rb
+++ rails/pingpong/app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
 	before_action :check_user_not_exist, only: [:show]
 
 	def index
-		@users = User.search(params[:search]).paginate(:per_page => 1, :page => params[:page])
+		@users = User.search(params[:search]).paginate(:per_page => 10, :page => params[:page])
 	end
 
 	def show
