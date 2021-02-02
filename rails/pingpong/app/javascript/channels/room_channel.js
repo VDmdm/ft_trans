3cdfafb120dfe266362b348cb7dc47d4f5ec75d6
deddara <amirhan16616@gmail.com>
2021-02-02 14:32:39 +0300

    jquerry fixes

--- rails/pingpong/app/javascript/channels/room_channel.js
+++ rails/pingpong/app/javascript/channels/room_channel.js
@@ -15,14 +15,20 @@ $(document).on("turbolinks:load", function() {
 		},
 		{
 		  received: function(data) {
+			var year = data.updated_at.substring(0, 10);
+			var time = data.updated_at.substring(11, 19) + " UTC";
 			var content = messageTemplate.children().clone(true, true);
 			content.find('[data-role="user-avatar"]').attr('src', data.user_avatar);
 			content.find('[data-role="user-id"]').attr('href', function() { return $(this).attr("href") + "/"+data.user_id});
 			content.find('[data-role="user-nickname"]').text(data.user_nickname);
-			content.find('[data-role="user-guild"]').text(data.user_guild);
-			content.find('[data-role="user-guild-id"]').attr('a', data.user_guild_id);
+			if (!data.user_guild)
+				content.find('[data-role="user-guild"]').css('display', 'hidden');
+			else{
+				content.find('[data-role="user-guild"]').text('[' + data.user_guild + ']');
+				content.find('[data-role="user-guild-id"]').attr('href', function() { return $(this).attr("href") + "/" + data.user_guild_id});
+			}
 			content.find('[data-role="message-text"]').text(data.message);
-			content.find('[data-role="message-date"]').text(data.updated_at);
+			content.find('[data-role="message-date"]').text(year + " " + time);
 			if (data.message.length != 0)
 				$element.append(content);
 			$element.animate({ scrollTop: $element.prop("scrollHeight")}, 1000);
