c9679b061d5d5153c6fb9d207e0786923fa6bd42
nikita <nikitos.athletos@gmail.com>
2021-02-10 01:15:32 +0300

    added fadeIn fadeOut

--- maket/assets/js/info_table.js
+++ maket/assets/js/info_table.js
@@ -1,42 +1,43 @@
 var Controller = Backbone.Router.extend({
     routes: {
-        "": "close_help_window", // Пустой hash-тэг
-        "help": "show_help_window", // Начальная страница
-        "help_site": "show_site", // Начальная страница
-        "help_game": "show_game", // Начальная страница
-        "help_guilds": "show_guilds", // Начальная страница
-        "help_tournament": "show_tournament", // Начальная страница
+        "help": "show_help_window",
+        "help_site": "show_site",
+        "help_game": "show_game",
+        "help_guilds": "show_guilds",
+        "help_tournament": "show_tournament",
     },
 
     show_help_window: function () {
-        $(".info-window").show(); // Прячем все блоки
+        $(".info-window").fadeIn("slow/400/fast");
     },
 
     show_site: function () {
-        $(".info-window-nav-item-info").hide(); // Прячем все блоки
-        $("#info-window-nav-item-site").show(); // Прячем все блоки
+        $(".info-window-nav-item-info").hide();
+        $("#info-window-nav-item-site").show();
     },
 
     show_game: function () {
-        $(".info-window-nav-item-info").hide(); // Прячем все блоки
-        $("#info-window-nav-item-game").show(); // Прячем все блоки
+        $(".info-window-nav-item-info").hide();
+        $("#info-window-nav-item-game").show();
     },
 
     show_guilds: function () {
-        $(".info-window-nav-item-info").hide(); // Прячем все блоки
-        $("#info-window-nav-item-guilds").show(); // Прячем все блоки
+        $(".info-window-nav-item-info").hide();
+        $("#info-window-nav-item-guilds").show();
     },
 
     show_tournament: function () {
-        $(".info-window-nav-item-info").hide(); // Прячем все блоки
-        $("#info-window-nav-item-tournament").show(); // Прячем все блоки
-    },
-
-    close_help_window: function () {
-        $(".info-window").hide(); // Прячем все блоки
+        $(".info-window-nav-item-info").hide();
+        $("#info-window-nav-item-tournament").show();
     },
 });
 
-var controller = new Controller(); // Создаём контроллер
+var controller = new Controller();
 
-Backbone.history.start();  // Запускаем HTML5 History push
+Backbone.history.start();
+
+$(document).ready(function () {
+    $('.info-window-close').click(function () {
+        $(".info-window").fadeOut("slow/400/fast");
+    });
+});
