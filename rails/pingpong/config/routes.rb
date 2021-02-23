f0232d07f549a82a59f1fb1daa5077e93f36c4f1
deddara <amirhan16616@gmail.com>
2021-02-23 07:05:03 +0300

    almost done

--- rails/pingpong/config/routes.rb
+++ rails/pingpong/config/routes.rb
@@ -62,7 +62,7 @@ Rails.application.routes.draw do
 
   get 'tournaments' , to: "tournaments#index"
 
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, controllers: { sessions: 'sessions', omniauth_callbacks: "users/omniauth_callbacks" }
 
   devise_scope :user do
     delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session_path
