e05f862225a4ac0c177364645cf551c39feb4950
nikita <nikitos.athletos@gmail.com>
2021-03-16 22:48:34 +0300

    added wars

--- maket/assets/js/games.js
+++ maket/assets/js/games.js
@@ -6,6 +6,11 @@
                 .addClass('active').siblings().removeClass('active')
                 .closest('div.games-types').find('div.tabs-content').removeClass('active').eq($(this).index()).addClass('active');
         });
+        $('ul.wars-sending-incoming-list').on('click', 'li:not(.active)', function() {
+            $(this)
+                .addClass('active').siblings().removeClass('active')
+                .closest('div.wars-sending-incoming').find('div.tabs-content').removeClass('active').eq($(this).index()).addClass('active');
+        });
 
     });
 })(jQuery);
