129bb01af21816a71ff5e9f61ec2d63e90c50bda
Snelk <dmitrytob@gmail.com>
2021-04-02 18:21:21 +0300

    + backbone js

---
+++ rails/pingpong/app/javascript/packs/info_table.js
@@ -0,0 +1,45 @@
+var Controller = Backbone.Router.extend({
+    routes: {
+        "help": "show_help_window",
+        "help_site": "show_site",
+        "help_game": "show_game",
+        "help_guilds": "show_guilds",
+        "help_tournament": "show_tournament",
+    },
+
+    show_help_window: function () {
+        $(".info-window").fadeIn("slow/400/fast");
+        $(".info-window-nav-item-info").hide();
+        $("#info-window-nav-item-site").show();
+    },
+
+    show_site: function () {
+        $(".info-window-nav-item-info").hide();
+        $("#info-window-nav-item-site").show();
+    },
+
+    show_game: function () {
+        $(".info-window-nav-item-info").hide();
+        $("#info-window-nav-item-game").show();
+    },
+
+    show_guilds: function () {
+        $(".info-window-nav-item-info").hide();
+        $("#info-window-nav-item-guilds").show();
+    },
+
+    show_tournament: function () {
+        $(".info-window-nav-item-info").hide();
+        $("#info-window-nav-item-tournament").show();
+    },
+});
+
+var controller = new Controller();
+
+$(document).on("turbolinks:load", function() {
+    $('.info-window-close').click(function () {
+        $(".info-window").fadeOut("slow/400/fast");
+    });
+    Backbone.history.stop();
+    Backbone.history.start();
+});
