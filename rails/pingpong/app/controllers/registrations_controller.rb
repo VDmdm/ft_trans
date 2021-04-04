3b4bda4d6ca0482364864f9aa88b659751d8dd64
deddara <deddara@student.21-school.ru>
2021-04-04 17:00:07 +0300

    draw added

--- rails/pingpong/app/controllers/registrations_controller.rb
+++ rails/pingpong/app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
 
 	protected
 	  def after_sign_up_path_for(resource)
-		edit_user_registration_path(resource)
+		edit_user_registration_path
 	  end
 	  
 	  def update_resource(resource, params)
